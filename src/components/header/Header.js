ccea4a1af4e429c4a8add00b8604d0731d7b5d14
rolero4 <72434958+Rolero4@users.noreply.github.com>
2023-01-24 12:31:26 +0100

    GMV-5: Add Basic ui for Header and Footer

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -2,7 +2,11 @@ import React from "react";
 import "./Header.css";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="header">
+      <div className="logo">tripleA</div>
+    </div>
+  );
 };
 
 export default Header;
