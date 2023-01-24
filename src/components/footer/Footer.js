e64940808bd767e8490569575b79f4b340c7c8e4
rolero4 <72434958+Rolero4@users.noreply.github.com>
2023-01-24 15:36:53 +0100

    GMV-5:  Add classNames for p

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -4,7 +4,7 @@ import "./Footer.css";
 const Footer = () => {
   return (
     <div className="footer">
-      <p>Author: tripleA</p>
+      <p className="authors">Author: tripleA</p>
       <p>
         <a href="mailto:tripleA@example.com">tripleA@example.com</a>
       </p>
