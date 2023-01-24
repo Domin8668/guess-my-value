9a3220882e2a27f8ed9edcd407e44be2dbc102de
rolero4 <72434958+Rolero4@users.noreply.github.com>
2023-01-24 22:34:03 +0100

    GMV-6:  Add lives and lives opacity

--- src/components/game/lives/Lives.js
+++ src/components/game/lives/Lives.js
@@ -1,8 +1,23 @@
 import React from "react";
 import "./Lives.css";
+import Life from "./life/Life";
 
-const Lives = () => {
-  return <div>Lives</div>;
+const Lives = ({ livesCount, livesLeft }) => {
+  // for (let index = 0; index < livesCount; index++) {
+  //   if(index<=livesLeft){
+  //     <Life opacity={false}
+  //   } else {
+  //     <Life opacity={true}/>
+  //   }
+  // }
+
+  return (
+    <div className="lives-container">
+      <Life opacity={false} />
+      <Life opacity={false} />
+      <Life opacity={true} />
+    </div>
+  );
 };
 
 export default Lives;
--- src/components/game/lives/life/Life.js
+++ src/components/game/lives/life/Life.js
@@ -1,8 +1,13 @@
 import React from "react";
 import "./Life.css";
 
-const Life = () => {
-  return <div>Life</div>;
+const Life = ({ opacity }) => {
+  let className = "heart";
+  if (opacity) {
+    className += " opacity";
+  }
+
+  return <div className={className}></div>;
 };
 
 export default Life;
