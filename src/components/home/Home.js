eb4c9593eb77597f4e3efe4930b16813a60b401c
rolero4 <72434958+Rolero4@users.noreply.github.com>
2023-01-29 16:26:34 +0100

    GMV-15:  Add queries for fold and landscape

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -45,66 +45,70 @@ const Home = () => {
   return (
     <>
       <div className="home-container">
-        <div className="records-container">
-          <div className="record-text">
-            Record on Easy:{" "}
-            <span className="score-number record-easy">{record.Easy}</span>
+        <div className="record-diff-container">
+          <div className="records-container">
+            <div className="record-text">
+              Record on Easy:{" "}
+              <span className="score-number record-easy">{record.Easy}</span>
+            </div>
+            <div className="record-text">
+              Record on Medium:{" "}
+              <span className="score-number record-medium">
+                {record.Medium}
+              </span>
+            </div>
+            <div className="record-text">
+              Record on Hard:{" "}
+              <span className="score-number record-hard">{record.Hard}</span>
+            </div>
           </div>
-          <div className="record-text">
-            Record on Medium:{" "}
-            <span className="score-number record-medium">{record.Medium}</span>
-          </div>
-          <div className="record-text">
-            Record on Hard:{" "}
-            <span className="score-number record-hard">{record.Hard}</span>
-          </div>
-        </div>
 
-        <div
-          className={`records-container radio-buttons-container ${backgroundColor}`}
-        >
-          <h3 className="record-text record-header">
-            Choose the difficulty level:
-          </h3>
-          <label className="rad-label" htmlFor="easy">
-            <input
-              className="rad-input"
-              type="radio"
-              name="level"
-              value="Easy"
-              id="easy"
-              checked={currentLevel === Level.Easy}
-              onChange={() => onLevelChange(Level.Easy)}
-            />
-            <div className="rad-design"></div>
-            <div className="rad-text">Easy</div>{" "}
-          </label>
-          <label className="rad-label" htmlFor="medium">
-            <input
-              className="rad-input"
-              type="radio"
-              name="level"
-              value="Medium"
-              id="medium"
-              checked={currentLevel === Level.Medium}
-              onChange={() => onLevelChange(Level.Medium)}
-            />
-            <div className="rad-design"></div>
-            <div className="rad-text">Medium</div>
-          </label>
-          <label className="rad-label" htmlFor="hard">
-            <input
-              className="rad-input"
-              type="radio"
-              name="level"
-              value="Hard"
-              id="hard"
-              checked={currentLevel === Level.Hard}
-              onChange={() => onLevelChange(Level.Hard)}
-            />
-            <div className="rad-design"></div>
-            <div className="rad-text">Hard</div>{" "}
-          </label>
+          <div
+            className={`records-container radio-buttons-container ${backgroundColor}`}
+          >
+            <h3 className="record-text record-header">
+              Choose the difficulty level:
+            </h3>
+            <label className="rad-label" htmlFor="easy">
+              <input
+                className="rad-input"
+                type="radio"
+                name="level"
+                value="Easy"
+                id="easy"
+                checked={currentLevel === Level.Easy}
+                onChange={() => onLevelChange(Level.Easy)}
+              />
+              <div className="rad-design"></div>
+              <div className="rad-text">Easy</div>{" "}
+            </label>
+            <label className="rad-label" htmlFor="medium">
+              <input
+                className="rad-input"
+                type="radio"
+                name="level"
+                value="Medium"
+                id="medium"
+                checked={currentLevel === Level.Medium}
+                onChange={() => onLevelChange(Level.Medium)}
+              />
+              <div className="rad-design"></div>
+              <div className="rad-text">Medium</div>
+            </label>
+            <label className="rad-label" htmlFor="hard">
+              <input
+                className="rad-input"
+                type="radio"
+                name="level"
+                value="Hard"
+                id="hard"
+                checked={currentLevel === Level.Hard}
+                onChange={() => onLevelChange(Level.Hard)}
+              />
+              <div className="rad-design"></div>
+              <div className="rad-text">Hard</div>{" "}
+            </label>
+          </div>
         </div>
         <div className="start-game-container">
           <FunctionButton
